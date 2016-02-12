c731789c60dd1ee1d8785fd2d0b17b216d3599db
Michael Arnold <valestrum@hotmail.com>
2016-02-11 23:34:31 -0500

    Created version 1.0.0

---
+++ YouTube_Reddit_Bot.py
@@ -0,0 +1,85 @@
+# Python 3.4
+from __future__ import unicode_literals
+import youtube_dl
+import re
+import requests
+import time
+import praw
+
+
+r = praw.Reddit(
+    user_agent='YouTube Reddit Bot - Version 1.0.0'
+               'Created by /u/Valestrum AKA /u/Killmail_Bot'
+               'Designed to find comments with YouTube links and '
+               'reply to the links with the title and length of the video.')
+username, password = [line.rstrip('\n') for line in open('user_info.txt')]
+r.login(username, password)
+subreddit = r.get_subreddit('test')
+loop_count = 0
+
+
+# Catch debug, warnings, and errors.
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+# Scrape YouTube details for each YouTube link found.
+def scrape_info(links):
+    output = []
+    ydl_opts = {'logger': MyLogger()}
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        for link in links:
+            info = ydl.extract_info(link, download=False)
+            title = info['title']
+            
+            seconds = info['duration']
+            m, s = divmod(seconds, 60)
+            h, m = divmod(m, 60)
+            duration = '{0}:{1}:{2}'.format(h, m, s)
+                
+            reply = '>"{0}" - Length: {1}'.format(title, duration)
+            output.append(reply)
+    return '\n\n'.join(output)
+
+
+def run_bot():
+    #Check existing comment ids.
+    with open('cache.txt','r') as cache:
+        existing = cache.read().splitlines()
+        
+    comments = subreddit.get_comments(limit=150)
+
+    with open('cache.txt', 'a+') as cache:
+        for comment in comments:
+            comment_text = comment.body
+
+            links = [
+                item for item in comment_text.split()
+                if re.match(r'https://www\.youtube\.com/watch\?v=.*', item)]
+
+            # Check that comment hasn't been seen before and that
+            # it meets the desired critera.
+            if links and comment.id not in existing:
+                    existing.append(comment.id)
+                    cache.write(comment.id + '\n')
+                    response = scrape_info(links)
+                    comment.reply(response)
+                    print('New comment found - ID: {0}'.format(comment.id))
+
+
+while True:
+    try:
+        run_bot()
+    except requests.ConnectionError as e:
+        print(e)
+    loop_count += 1
+    print("Program loop #{0} completed successfully.".format(loop_count))
+    time.sleep(900) # Once every 15 minutes.
