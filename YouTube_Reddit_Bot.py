7a9c1e95affa59eef5730d109cbfc5bd425a7c64
Michael Arnold <valestrum@hotmail.com>
2016-02-26 20:46:38 -0500

    v1.1.1 - Changed title to non-hyperlink

--- YouTube_Reddit_Bot.py
+++ YouTube_Reddit_Bot.py
@@ -8,7 +8,7 @@ import praw         # Reddit interaction
 
 
 r = praw.Reddit(
-    user_agent='YouTube Reddit Bot - Version 1.1.0'
+    user_agent='YouTube Reddit Bot - Version 1.1.1'
                'Created by /u/Valestrum AKA /u/Killmail_Bot'
                'Designed to find comments with YouTube links and '
                'reply to the links with the title and length of the video.')
@@ -38,7 +38,8 @@ def scrape_info(links):
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         for link in links:
             info = ydl.extract_info(link, download=False)
-            title = "[{0}]({1})".format(info['title'], link)
+            title = info['title'] # Title without hyperlink (Which seems to be preferred?)
+            #title = "[{0}]({1})".format(info['title'], link) # Used for hyperlinking title
             seconds = info['duration']
             duration = time.strftime('%H:%M:%S', time.gmtime(seconds))
             reply = '>"{0}" - Length: {1}'.format(title, duration)
